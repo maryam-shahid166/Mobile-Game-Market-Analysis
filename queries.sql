068cb993feaee4146fa6028c1fd0216d78cd246d
Maryam Shahid <maryamshahid16610@gmail.com>
2026-09-08 23:25:26 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,21 @@
+Query 1:
+  
+SELECT App,
+       CAST(REPLACE(REPLACE(Installs, ',', ''), '+', '') AS INTEGER) AS Installs_Clean
+FROM games;
 
+Query 2:
+  
+SELECT Type, ROUND(AVG(Rating),2) AS Avg_Rating, COUNT(*) AS Num_Apps
+FROM games
+WHERE Rating IS NOT NULL
+GROUP BY Type;
+
+Query 3:
+  
+SELECT Genres,
+       ROUND(AVG(Rating),2) AS Avg_Rating,
+       AVG(CAST(REPLACE(REPLACE(Installs,',',''),'+','') AS INTEGER)) AS Avg_Installs
+FROM games
+GROUP BY Genres
+ORDER BY Avg_Rating DESC;
